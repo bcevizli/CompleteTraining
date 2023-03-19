b79cd9b63a56d1433d9a8df208e97940f053e3eb
burak cevizli <burak.cevizli7@gmail.com>
2023-03-20 02:07:02 +0300

    Personal Information Try

--- CompleteTraining/Controllers/ProfileController/TrainingPrograms/Personal Information/PersonalInformationViewController.swift
+++ CompleteTraining/Controllers/ProfileController/TrainingPrograms/Personal Information/PersonalInformationViewController.swift
@@ -7,15 +7,54 @@
 
 import UIKit
 
-class PersonalInformationViewController: UIViewController {
-
+class PersonalInformationViewController: UIViewController, UITextFieldDelegate {
+    
+    let lbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 50, y: 100, width: 100, height: 30))
+        lbl.text = "Label"
+        return lbl
+    }()
+    
+    let textF: UITextField = {
+        let textF = UITextField(frame: CGRect(x: 50, y: 150, width: 100, height: 30))
+        textF.borderStyle = .roundedRect
+        textF.isHidden = true
+        return textF
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Personal Information"
+        
+        textF.delegate = self
+        
+        view.addSubview(lbl)
+        view.addSubview(textF)
+        
+        lbl.isUserInteractionEnabled = true
+        
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(lblTapped))
+        tapGesture.numberOfTapsRequired = 1
+        lbl.addGestureRecognizer(tapGesture)
     }
     
+    @objc func lblTapped() {
+        lbl.isHidden = true
+        textF.isHidden = false
+        textF.text = "text here"
+        
+    }
+    
+    func textFieldShouldReturn(_ userText: UITextField) -> Bool {
+        textF.resignFirstResponder()
+        textF.isHidden = true
+        lbl.isHidden = false
+        lbl.text = textF.text
 
-  
-
+        return true
+    }
+    
+    
 }
